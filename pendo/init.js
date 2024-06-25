9ee7ac80c2f4bdbdda2a7a719941bef3237e8d35
darshanhu <133042615+darshanhu@users.noreply.github.com>
2024-06-25 16:58:50 +0530

    Pendo installed

---
+++ pendo/init.js
@@ -0,0 +1,23 @@
+(function(apiKey){
+    (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=o._q||[];
+    v=['initialize','identify','updateOptions','pageLoad','track'];for(w=0,x=v.length;w<x;++w)(function(m){
+        o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
+        y=e.createElement(n);y.async=!0;y.src='https://cdn.pendo.io/agent/static/'+apiKey+'/pendo.js';
+        z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
+
+        // This function creates visitors and accounts in Pendo
+        // You will need to replace <visitor-id-goes-here> and <account-id-goes-here> with values you use in your app
+        // Please use Strings, Numbers, or Bools for value types.
+        pendo.initialize({
+            visitor: {
+                id: "no-account-id-visitor",
+                name: "Visitor with No Account ID"
+                
+            },
+        
+            account: {
+                id: "",
+                name: "no-account-id-visitor",
+            },
+        });
+})('43994d6b-66ea-49fc-71f1-31077e387150');
